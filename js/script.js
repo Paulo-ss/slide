321b2a79f6c1b14928db9dfb63019871c49ae332
Paulo Silveira <paulossnjf@gmail.com>
2020-12-17 14:25:48 -0300

    Controles de navegação adicionados

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ import { SlideNav } from './modules/slide.js';
 
 const slideNav = new SlideNav('.slide', '.slide-wrapper');
 slideNav.init();
-
 slideNav.addArrow('.prev', '.next');
+slideNav.addControl();
